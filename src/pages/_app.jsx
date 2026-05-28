34ce0677fc6b6c2fc52c64b62b31038b49109e36
achul123 <thetheatregaming@gmail.com>
2026-05-28 08:16:05 +0200

    update(seo): move metadata to app and update robots

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { slugifyWithCounter } from '@sindresorhus/slugify'
 
 import { Layout } from '@/components/Layout'
@@ -49,13 +50,21 @@ function collectHeadings(nodes, slugify = slugifyWithCounter()) {
 }
 
 export default function App({ Component, pageProps }) {
+  let router = useRouter()
   let title = pageProps.markdoc?.frontmatter.title
 
   let pageTitle =
     pageProps.markdoc?.frontmatter.pageTitle ||
-    `${pageProps.markdoc?.frontmatter.title} - Docs`
+    (title ? `${title} - Skyport` : 'Skyport Documentation')
 
-  let description = pageProps.markdoc?.frontmatter.description
+  let description =
+    pageProps.markdoc?.frontmatter.description ||
+    'Skyport Documentation - Guide and API Reference'
+
+  let url = `https://skyport.dev${
+    router.asPath === '/' ? '' : router.asPath.split('?')[0]
+  }`
+  let image = 'https://i.imgur.com/llwMKX4.png'
 
   let tableOfContents = pageProps.markdoc?.content
     ? collectHeadings(pageProps.markdoc.content)
@@ -65,7 +74,21 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>{pageTitle}</title>
-        {description && <meta name="description" content={description} />}
+        <meta name="description" content={description} />
+        
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={pageTitle} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image} />
+
+        <link rel="canonical" href={url} />
       </Head>
       <Layout title={title} tableOfContents={tableOfContents}>
         <Component {...pageProps} />
